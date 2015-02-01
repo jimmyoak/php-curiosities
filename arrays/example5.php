6fec124e3b1f012cdbbd9b6dc2380a875f6e90fe
roger.codina <roger.codina@atrapalo.com>
2015-02-01 21:04:04 +0100

    basics example1

--- arrays/example5.php
+++ arrays/example5.php
@@ -1,22 +1,5 @@
 <?php
 
-/**
- * @author Fernando Villar
- */
-
-//Result:
-/*
-
-array(3) {
-  [0] =>
-  string(4) "zero"
-  [1] =>
-  string(3) "one"
-  [2] =>
-  string(3) "one" // <-------------------------------
-}
-*/
-
 $a = array('zero', 'one', 'two');
 
 foreach ($a as &$v) {
---
+++ basics/example1.php
@@ -0,0 +1,16 @@
+<?php
+
+function test()
+{
+    static $count = 0;
+
+    $count++;
+    echo $count;
+    if ($count < 10) {
+        test();
+    }
+    $count--;
+}
+
+test();
+test();
