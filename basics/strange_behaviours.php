d6b4018cbe75eeaaddbf37d0c91de36e9f2cf175
roger.codina <roger.codina@atrapalo.com>
2015-02-01 23:50:52 +0100

    fix

--- basics/strange_behaviours.php
+++ basics/strange_behaviours.php
@@ -7,6 +7,6 @@ echo 'Números con strings (cast a float de "7.45e6hola"): ' . ((float)'7.45e6ho
 echo 'Números con strings (cast a float de "7.hola45"): ' . ((float)'7.hola45') . "\n";
 echo 'Comparativas 5 == "5hola": ' . (5 == '5hola' ? 'true' : 'false') . "\n";
 echo 'Comparativas "5hola" == 5: ' . ('5hola' == 5 ? 'true' : 'false') . "\n";
-echo 'Comparativas "5" == "5hola": ' . ("5" == '5hola' ? 'true' : 'false') . "\n";
+echo 'Comparativas "5" == "5hola": ' . ('5' == '5hola' ? 'true' : 'false') . "\n";
 echo 'Comparativas 5 == "7hola": ' . (5 == '7hola' ? 'true' : 'false') . "\n";
 echo 'Comparativas 5 == "hola8": ' . (5 == 'hola8' ? 'true' : 'false') . "\n";
